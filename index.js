69c3ccd57817eee319a9e620b8352acf97ef2806
Bruce Harris <bharris845@gmail.com>
2012-10-05 09:20:51 -0400

    before demo

--- index.js
+++ index.js
@@ -53,26 +53,43 @@ $(function() {
     */
 
 
-    var symbol = $(this).val(),
-        $results = $('<div'
+    var symbol = $(this).val();
+    // TODO find existing element
 
+    $('#results').append('<div class="resultset sym-' + symbol + '"/>');
+    var $result = $('#results .resultset.sym-' + symbol);
+
+    $result.append('<div class="symbol"><strong>Symbol:</strong>  <img src=https://si0.twimg.com/a/1349296073/images/resources/twitter-bird-light-bgs.png><span> $' + symbol + '</span></symbol>');
+    $result.append('<div>Mentions: <span class="mention"> </span> </div>');
+    $result.append('<div>Positives: <span class="positive"></span> </div>');
+    $result.append('<div>Negative: <span class="negative"></span> </div>');
+
+    $(this).val('');    
+
+    var counts = {
+      mention: 0,
+      positive: 0,
+      negative: 0
+    }
 
     function makeUpdater(type) {
-      var $el = $("." + type);
+      var $el = $result.find("." + type);
 
-      return function (count){
-        $el.html(count);
+      return function (){
+        console.log(type);
+        $el.html(++counts[type]);
       };
 
     }
 
-    var socket = io.connect('http://localhost:3010');
-    socket.emit('follow', {symbol: symbol });
-    socket.on('mention:' + symbol, updateMention);
-    socket.on('positive:' + symbol, updatePositive);
-    socket.on('negative:' + symbol, updateNegative);
+    //var socket = io.connect('http://localhost:8010');
+    var socket = io.connect('http://10.0.0.192:8010');
+    socket.emit('follow', {symbol: "$" + symbol });
+    socket.on('mention:$' + symbol, makeUpdater('mention'));
+    socket.on('positive:$' + symbol, makeUpdater('positive'));
+    socket.on('negative:$' + symbol, makeUpdater('negative'));
 
-
+/*
 
     run($(this).val(), function(result) {
       console.log(result);
@@ -82,5 +99,6 @@ $(function() {
       $results.find('.negative').html(result.ncount);
       $results.show().removeClass("loading");
     });    
+*/
   });
 });
